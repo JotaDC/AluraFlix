7d39748a9dfd7aaa7fbad006056795b9267d6caa
JotaDC <javierdariocanete@gmail.com>
2025-01-11 04:44:00 -0300

    modalPlay

--- src/components/ListaVideos/ListaVideos.jsx
+++ src/components/ListaVideos/ListaVideos.jsx
@@ -1,9 +1,15 @@
+// import React, { useEffect, useState } from "react";
+// import { getVideos, deleteVideo, updateVideo } from "../../services/servicesApi/servicesApi";
+// import ModalConfirmar from "../ModalConfirmar/ModalConfirmar";
+// import ModalEditar from "../ModalEditar/ModalEditar"
+// import styles from "./ListaVideos.module.css";
+
 import React, { useEffect, useState } from "react";
 import { getVideos, deleteVideo, updateVideo } from "../../services/servicesApi/servicesApi";
 import ModalConfirmar from "../ModalConfirmar/ModalConfirmar";
-import ModalEditar from "../ModalEditar/ModalEditar"
+import ModalEditar from "../ModalEditar/ModalEditar";
+import ModalPlay from "../ModalPlay/ModalPlay";  // Importa el nuevo modal
 import styles from "./ListaVideos.module.css";
-
 
 const ListaVideos = ({ categoria, color }) => {
   const [videos, setVideos] = useState([]);
@@ -11,8 +17,10 @@ const ListaVideos = ({ categoria, color }) => {
   const [loading, setLoading] = useState(true);
   const [modalEliminarVisible, setModalEliminarVisible] = useState(false);
   const [modalEditarVisible, setModalEditarVisible] = useState(false);
+  const [modalPlayVisible, setModalPlayVisible] = useState(false);  // Estado para el modal de reproducción
   const [videoToDelete, setVideoToDelete] = useState(null);
   const [videoToEdit, setVideoToEdit] = useState(null);
+  const [videoToPlay, setVideoToPlay] = useState(null);  // Estado para el video que se va a reproducir
 
   useEffect(() => {
     const fetchVideos = async () => {
@@ -27,16 +35,8 @@ const ListaVideos = ({ categoria, color }) => {
     };
 
     fetchVideos();
-  }, [filteredVideos]);
+  }, [videos]);
 
-  // useEffect(() => {
-  //   if (categoria) {
-  //     setFilteredVideos(videos.filter((video) => video.categoria === categoria));
-  //   } else {
-  //     setFilteredVideos(videos);
-  //   }
-  // }, [videos, categoria]);
-  
   useEffect(() => {
     if (categoria) {
       setFilteredVideos(videos.filter((video) => video.categoria === categoria));
@@ -44,7 +44,7 @@ const ListaVideos = ({ categoria, color }) => {
       setFilteredVideos(videos);
     }
   }, [videos, categoria]);
-  
+
   const handleEliminarClick = (id) => {
     setVideoToDelete(id);
     setModalEliminarVisible(true);
@@ -72,45 +72,14 @@ const ListaVideos = ({ categoria, color }) => {
     setModalEditarVisible(true);
   };
 
-  // const handleGuardarEditar = async (data) => {
-  //   try {
-  //     const updatedVideo = await updateVideo(videoToEdit.id, data);
-  //     setVideos(
-  //       videos.map((video) =>
-  //         video.id === videoToEdit.id ? { ...updatedVideo } : video
-  //       )
-  //     );
-  //   } catch (error) {
-  //     console.error("Error al editar el video:", error);
-  //   } finally {
-  //     setModalEditarVisible(false);
-  //     setVideoToEdit(null);
-  //   }
-  // };
   const handleGuardarEditar = async (data) => {
     try {
       const updatedVideo = await updateVideo(videoToEdit.id, data);
-  
-      // Actualizar el estado general de videos
       setVideos(
         videos.map((video) =>
           video.id === videoToEdit.id ? { ...updatedVideo } : video
         )
       );
-  
-      // Si la categoría actual coincide, actualizamos también la lista filtrada
-      if (!categoria || updatedVideo.categoria === categoria) {
-        setFilteredVideos((prev) =>
-          prev.map((video) =>
-            video.id === videoToEdit.id ? { ...updatedVideo } : video
-          )
-        );
-      } else {
-        // Si el video editado ya no pertenece a la categoría actual, lo eliminamos de la lista filtrada
-        setFilteredVideos((prev) =>
-          prev.filter((video) => video.id !== videoToEdit.id)
-        );
-      }
     } catch (error) {
       console.error("Error al editar el video:", error);
     } finally {
@@ -124,11 +93,22 @@ const ListaVideos = ({ categoria, color }) => {
     setVideoToEdit(null);
   };
 
+  // Manejo del clic para abrir el modal de reproducción
+  const handlePlayVideo = (video) => {
+    setVideoToPlay(video);
+    setModalPlayVisible(true);
+  };
+
+  const handleCerrarModalPlay = () => {
+    setModalPlayVisible(false);
+    setVideoToPlay(null);
+  };
+
   if (loading) return <p>Cargando videos...</p>;
 
   return (
     <div className={styles.container}>
-      <h2 className={styles.title} style={{color:color}}>
+      <h2 className={styles.title} style={{ color: color }}>
         {categoria ? `Videos de ${categoria}` : "Lista de Videos"}
       </h2>
       <div className={styles.cards}>
@@ -137,13 +117,12 @@ const ListaVideos = ({ categoria, color }) => {
             <img
               src={video.imagen_url || "./img/sinimagen.png"}
               alt={video.titulo}
+              onClick={() => handlePlayVideo(video)}  // Al hacer clic en la imagen, se abre el modalPlay
             />
             <h3>{video.titulo}</h3>
             <div>
               <button onClick={() => handleEditarClick(video)}>Editar</button>
-              <button onClick={() => handleEliminarClick(video.id)}>
-                Eliminar
-              </button>
+              <button onClick={() => handleEliminarClick(video.id)}>Eliminar</button>
             </div>
           </div>
         ))}
@@ -164,11 +143,184 @@ const ListaVideos = ({ categoria, color }) => {
           onCancelar={handleCancelarEditar}
         />
       )}
+
+      {modalPlayVisible && videoToPlay && (
+        <ModalPlay
+          video={videoToPlay}  // Pasa el video que se quiere reproducir al modalPlay
+          onCerrar={handleCerrarModalPlay}  // Función para cerrar el modal
+        />
+      )}
     </div>
   );
 };
 
 export default ListaVideos;
+
+
+// const ListaVideos = ({ categoria, color }) => {
+//   const [videos, setVideos] = useState([]);
+//   const [filteredVideos, setFilteredVideos] = useState([]);
+//   const [loading, setLoading] = useState(true);
+//   const [modalEliminarVisible, setModalEliminarVisible] = useState(false);
+//   const [modalEditarVisible, setModalEditarVisible] = useState(false);
+//   const [videoToDelete, setVideoToDelete] = useState(null);
+//   const [videoToEdit, setVideoToEdit] = useState(null);
+
+//   useEffect(() => {
+//     const fetchVideos = async () => {
+//       try {
+//         const data = await getVideos();
+//         setVideos(data);
+//       } catch (error) {
+//         console.error("Error al cargar los videos:", error);
+//       } finally {
+//         setLoading(false);
+//       }
+//     };
+
+//     fetchVideos();
+//   }, [filteredVideos]);
+
+//   // useEffect(() => {
+//   //   if (categoria) {
+//   //     setFilteredVideos(videos.filter((video) => video.categoria === categoria));
+//   //   } else {
+//   //     setFilteredVideos(videos);
+//   //   }
+//   // }, [videos, categoria]);
+  
+//   useEffect(() => {
+//     if (categoria) {
+//       setFilteredVideos(videos.filter((video) => video.categoria === categoria));
+//     } else {
+//       setFilteredVideos(videos);
+//     }
+//   }, [videos, categoria]);
+  
+//   const handleEliminarClick = (id) => {
+//     setVideoToDelete(id);
+//     setModalEliminarVisible(true);
+//   };
+
+//   const handleConfirmarEliminar = async () => {
+//     try {
+//       await deleteVideo(videoToDelete);
+//       setVideos(videos.filter((video) => video.id !== videoToDelete));
+//     } catch (error) {
+//       console.error("Error al eliminar el video:", error);
+//     } finally {
+//       setModalEliminarVisible(false);
+//       setVideoToDelete(null);
+//     }
+//   };
+
+//   const handleCancelarEliminar = () => {
+//     setModalEliminarVisible(false);
+//     setVideoToDelete(null);
+//   };
+
+//   const handleEditarClick = (video) => {
+//     setVideoToEdit(video);
+//     setModalEditarVisible(true);
+//   };
+
+//   // const handleGuardarEditar = async (data) => {
+//   //   try {
+//   //     const updatedVideo = await updateVideo(videoToEdit.id, data);
+//   //     setVideos(
+//   //       videos.map((video) =>
+//   //         video.id === videoToEdit.id ? { ...updatedVideo } : video
+//   //       )
+//   //     );
+//   //   } catch (error) {
+//   //     console.error("Error al editar el video:", error);
+//   //   } finally {
+//   //     setModalEditarVisible(false);
+//   //     setVideoToEdit(null);
+//   //   }
+//   // };
+//   const handleGuardarEditar = async (data) => {
+//     try {
+//       const updatedVideo = await updateVideo(videoToEdit.id, data);
+  
+//       // Actualizar el estado general de videos
+//       setVideos(
+//         videos.map((video) =>
+//           video.id === videoToEdit.id ? { ...updatedVideo } : video
+//         )
+//       );
+  
+//       // Si la categoría actual coincide, actualizamos también la lista filtrada
+//       if (!categoria || updatedVideo.categoria === categoria) {
+//         setFilteredVideos((prev) =>
+//           prev.map((video) =>
+//             video.id === videoToEdit.id ? { ...updatedVideo } : video
+//           )
+//         );
+//       } else {
+//         // Si el video editado ya no pertenece a la categoría actual, lo eliminamos de la lista filtrada
+//         setFilteredVideos((prev) =>
+//           prev.filter((video) => video.id !== videoToEdit.id)
+//         );
+//       }
+//     } catch (error) {
+//       console.error("Error al editar el video:", error);
+//     } finally {
+//       setModalEditarVisible(false);
+//       setVideoToEdit(null);
+//     }
+//   };
+
+//   const handleCancelarEditar = () => {
+//     setModalEditarVisible(false);
+//     setVideoToEdit(null);
+//   };
+
+//   if (loading) return <p>Cargando videos...</p>;
+
+//   return (
+//     <div className={styles.container}>
+//       <h2 className={styles.title} style={{color:color}}>
+//         {categoria ? `Videos de ${categoria}` : "Lista de Videos"}
+//       </h2>
+//       <div className={styles.cards}>
+//         {filteredVideos.map((video) => (
+//           <div key={video.id} className={styles.card}>
+//             <img
+//               src={video.imagen_url || "./img/sinimagen.png"}
+//               alt={video.titulo}
+//             />
+//             <h3>{video.titulo}</h3>
+//             <div>
+//               <button onClick={() => handleEditarClick(video)}>Editar</button>
+//               <button onClick={() => handleEliminarClick(video.id)}>
+//                 Eliminar
+//               </button>
+//             </div>
+//           </div>
+//         ))}
+//       </div>
+
+//       {modalEliminarVisible && (
+//         <ModalConfirmar
+//           mensaje="¿Estás seguro de que deseas eliminar este video?"
+//           onConfirmar={handleConfirmarEliminar}
+//           onCancelar={handleCancelarEliminar}
+//         />
+//       )}
+
+//       {modalEditarVisible && videoToEdit && (
+//         <ModalEditar
+//           video={videoToEdit}
+//           onGuardar={handleGuardarEditar}
+//           onCancelar={handleCancelarEditar}
+//         />
+//       )}
+//     </div>
+//   );
+// };
+
+// export default ListaVideos;
 
 
 // const ListaVideos = ({ categoria }) => {
---
+++ src/components/ModalPlay/ModalPlay.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import styles from "./ModalPlay.module.css";
+
+const ModalPlay = ({ video, onCerrar }) => {
+  return (
+    <div className={styles.modal}>
+      <div className={styles.modalContent}>
+        <button className={styles.closeButton} onClick={onCerrar}>
+        X
+        </button>
+        <h2>{video.url}</h2>
+        <div className={styles.videoContainer}>
+         
+        <iframe
+            width="100%"
+            height="315"
+            src={video.video_url}  // Asegúrate de que el video tenga la propiedad 'url' para reproducirlo
+            title={video.titulo}
+            frameBorder="0"
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ModalPlay;
